d12daf5bf7f37930313ed1e33f8b13414c1252d7
Ivan Kuruch <kuruch2004@gmail.com>
2024-08-18 16:22:11 +0300

    update models

---
+++ starchat/models/comment.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from starchat.models.post import Post
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=5000)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
--- starchat/models/post.py
+++ starchat/models/post.py
@@ -5,9 +5,8 @@ from django.db import models
 class Post(models.Model):
     text = models.CharField(max_length=5000)
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-
-class Comment(models.Model):
-    text = models.CharField(max_length=5000)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    class Meta:
+        ordering = ['-created_at']
